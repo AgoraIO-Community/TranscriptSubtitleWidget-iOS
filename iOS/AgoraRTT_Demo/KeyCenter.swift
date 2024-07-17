d01487b79333d8500e8ea32e1d2d8d001b6fac8d
ZYP <954468540@qq.com>
2024-07-17 10:55:14 +0800

    add key center

---
+++ iOS/AgoraRTT_Demo/KeyCenter.swift
@@ -0,0 +1,21 @@
+//
+//  KeyCenter.swift
+//  AgoraRTT_Demo
+//
+//  Created by ZYP on 2024/6/21.
+//
+
+import Foundation
+
+struct KeyCenter {
+    static let appId: String = <#Your App Id#>
+    
+    static let serverEnvs: [ServerEnv] = [
+        ServerEnv(name: "rtt server",
+                  serverUrlString: <#Your Server URL#>,
+                  testIp: <#Your Test IP#>,
+                  testPort: <#Your Test Port#>),
+    ]
+}
+
+
