b6ed6930299755f96d9e83516357b1cf73fbd7c0
ZYP <954468540@qq.com>
2024-07-30 11:29:03 +0800

    update http

--- AgoraRTT_Demo/MainVC/Manager/HttpClient.swift
+++ AgoraRTT_Demo/MainVC/Manager/HttpClient.swift
@@ -48,7 +48,7 @@ class HttpClient: NSObject {
                 let respDict = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
                 guard let token = respDict["tokenName"] as? String else {
-                    Log.errorText(text: "resp: \(respDict)", tag: logTag)
+                    Log.errorText(text: "acquire resp: \(respDict)", tag: logTag)
                     completed(nil, "Failed to acquire token, can not find token in response")
                     return
                 }
